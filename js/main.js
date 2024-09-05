4ce0832486680e71937860af455d73f6b90b2f1b
santiago <santiagocastillo766@gmail.com>
2024-09-05 19:16:34 -0300

    cambios en el html y se añadio JS

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const btnSwal = document.getElementById("botonSwal"),
+btnToast = document.getElementById("botonToast"),
+horaActual = document.querySelector("#horaActual"),
+fechaActual = document.querySelector (".fecha-actual");
+
+
+const inputs = document.querySelectorAll("input")
