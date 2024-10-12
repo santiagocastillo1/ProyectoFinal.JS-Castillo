a1a86d3c90d373c5c4a88e21c0008b80a930458e
santiago <santiagocastillo766@gmail.com>
2024-10-12 14:28:45 -0300

    cambios en main.js

--- js/main.js
+++ js/main.js
@@ -1,30 +1,6 @@
 const btnSwal = document.getElementById("botonSwal"),
-btnToast = document.getElementById("botonToast"),
-horaActual = document.querySelector("#horaActual"),
-fechaActual = document.querySelector (".fecha-actual");
+  btnToast = document.getElementById("botonToast"),
+  horaActual = document.querySelector("#horaActual"),
+  fechaActual = document.querySelector(".fecha-actual");
 
-
-
-const inputs = document.querySelectorAll("input")
-
-const producto = {
-
-    nombre: "Papas",
-    precio: 230,
-  
-  };
-  
-  let concatenado =
-  
-    "Producto: " + producto.nombre + ", precio: $" + producto.precio;
-  
-    let template= `PRODUCTO: ${producto.nombre}, 
-  
-    PRECIO: $ ${producto.precio}`;//interpolacion
-  
-  // contenedor.innerText = concatenado;
-  
-  principl.innerText = template;
-
-let nombre= "santi"
-console.log(nombre);
+const inputs = document.querySelectorAll("input");
