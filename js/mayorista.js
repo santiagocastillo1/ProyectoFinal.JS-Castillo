b4f693d5ada868605bbb76b0de0f26cb5b77943f
santiago <santiagocastillo766@gmail.com>
2024-10-13 16:27:20 -0300

    js e index html cambios en el navbar

---
+++ js/mayorista.js
@@ -0,0 +1,9 @@
+const btnMayorista = document.getElementById("btnMayorista");
+btnMayorista.addEventListener("click", function () {
+  Swal.fire({
+    icon: "error",
+    title: "ZONA NO HABILITADA",
+    footer:
+      '<a href="https://www.instagram.com/efedepe.dg/">ESCRIBINOS ACÁ</a>',
+  });
+});
--- js/register.js
+++ js/register.js
@@ -2,20 +2,20 @@ const btnRegister = document.getElementById("btnRegister");
 
 btnRegister.addEventListener("click", async () => {
   const { value: email } = await Swal.fire({
-    title: "Input email address",
+    title: "Ingresa tu email.",
     input: "email",
-    inputLabel: "Your email address",
-    inputPlaceholder: "Enter your email address",
+    //inputLabel: "Tu codigo email.",
+    inputPlaceholder: "Ingrese su email aqui.",
   });
 
   if (email) {
     Swal.fire(`Entered email: ${email}`);
   }
   const { value: password } = await Swal.fire({
-    title: "Enter your password",
+    title: "Contraseña",
     input: "password",
-    inputLabel: "Password",
-    inputPlaceholder: "Enter your password",
+    //inputLabel: "Password",
+    inputPlaceholder: "Ingrese tu contraseña aqui.",
     inputAttributes: {
       maxlength: "10",
       autocapitalize: "off",
@@ -25,4 +25,19 @@ btnRegister.addEventListener("click", async () => {
   if (password) {
     Swal.fire(`Entered password: ${password}`);
   }
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 1500,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    },
+  });
+  Toast.fire({
+    icon: "success",
+    title: "Registrado Correctamente.",
+  });
 });
