c069100388b6ae564e62ae1e2b075178b9ba720b
santiago <santiagocastillo766@gmail.com>
2024-10-14 00:28:34 -0300

    service

--- js/main.js
+++
@@ -1,8 +0,0 @@
-//fetch
-const inputSearch = document.querySelector("#buscar");
-
-fetch("./data/servicios.json")
-  .then((response) => response.json())
-  .then((servicio) => {
-    console.log(servicio);
-  });
